5d81bce56caeddb8489cc2e0a219ace423797f7e
mjjolnir <mjjolnir@gmail.com>
2015-01-13 12:30:23 +0200

    rev 03

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
 
-setTimeout(function() {
-            navigator.splashscreen.hide();
-}, 6000);
+var app = {
 
-alert("deneme");
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    
+    receivedEvent: function(id) {
+    	setTimeout(function() {
+            navigator.splashscreen.hide();
+    	}, 4000);
+    	
+        navigator.notification.confirm(
+        	    'You are the winner!', // message
+        	     this.onConfirm,            // callback to invoke with index of button pressed
+        	    'Game Over',           // title
+        	    ['Restart','Exit']     // buttonLabels
+        	);
+        	
+        console.log('Received Event: ' + id);
+    },
+    
+    onConfirm: function(buttonIndex) {
+        alert('You selected button ' + buttonIndex);
+    }
+};
+
+app.initialize();
